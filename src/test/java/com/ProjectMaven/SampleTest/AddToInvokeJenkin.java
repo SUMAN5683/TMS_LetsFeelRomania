5d5d02f434898afee0dc519cd28069887b687d58
suman <suman@INBook_X1>
2023-01-23 12:22:01 +0530

    pushed

---
+++ src/test/java/com/ProjectMaven/SampleTest/AddToInvokeJenkin.java
@@ -0,0 +1,11 @@
+package com.ProjectMaven.SampleTest;
+
+import org.testng.annotations.Test;
+
+public class AddToInvokeJenkin {
+	@Test(groups = "smoke")
+	public void test() {
+		System.out.println("ok tested");
+	}
+
+}
